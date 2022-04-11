d54f1a6f14c2fc1d0a8ef07e354888c8de7141ba
achakka007 <51894682+achakka007@users.noreply.github.com>
2022-04-11 18:28:04 -0400

    Add comment text box

--- code-review/src/components/movies.jsx
+++ code-review/src/components/movies.jsx
@@ -8,55 +8,58 @@ import page4 from '../data/page4.txt';
 class Pages extends Component {
     state = {
         pages: [
-            { title: 'example.txt' },
-            { title: 'page1.txt' },
-            { title: 'page2.txt' },
-            { title: 'page3.txt' },
-            { title: 'page4.txt' },
+            { title: 'example.txt', comment: '' },
+            { title: 'page1.txt', comment: '' },
+            { title: 'page2.txt', comment: '' },
+            { title: 'page3.txt', comment: '' },
+            { title: 'page4.txt', comment: '' },
         ],
         selection: "example.txt",
-        code: ["example code"]
+        code: ["example code"],
+        comment: "example comment"
     }
 
     handlePageSelect = (selection) => {
+
+        var page = this.state.pages.find(p => p.title === selection);
 
         switch (selection) {
             case 'page1.txt':
                 fetch(page1)
                     .then(r => r.text())
                     .then(text => {
-                        this.setState({ pages: this.state.pages, selection: selection, code: text.split("\n") })
+                        this.setState({ ...this.state, selection: selection, code: text.split("\n"), comment: page.comment })
                     });
                 break;
             case 'page2.txt':
                 fetch(page2)
                     .then(r => r.text())
                     .then(text => {
-                        this.setState({ pages: this.state.pages, selection: selection, code: text.split("\n") })
+                        this.setState({ ...this.state, selection: selection, code: text.split("\n"), comment: page.comment })
                     });
                 break;
             case 'page3.txt':
                 fetch(page3)
                     .then(r => r.text())
                     .then(text => {
-                        this.setState({ pages: this.state.pages, selection: selection, code: text.split("\n") })
+                        this.setState({ ...this.state, selection: selection, code: text.split("\n"), comment: page.comment })
                     });
                 break;
             case 'page4.txt':
                 fetch(page4)
                     .then(r => r.text())
                     .then(text => {
-                        this.setState({ pages: this.state.pages, selection: selection, code: text.split("\n") })
+                        this.setState({ ...this.state, selection: selection, code: text.split("\n"), comment: page.comment })
                     });
                 break;
             default:
-                this.setState({ pages: this.state.pages, selection: "example.txt", code: ["example code"] })
+                this.setState({ ...this.state, selection: "example.txt", code: ["example code"], comment: "example comment" })
         }
+    }
 
-        console.log(this.state.code);
-
-
-
+    handleCommentChange = event => {
+        const input = event.target.value;
+        this.setState({ ...this.state, pages: this.state.pages.map(p => (p.title === this.state.selection ? { ...p, comment: input } : p)), comment: input });
     }
 
     render() {
@@ -80,6 +83,9 @@ class Pages extends Component {
                         <h1>{currentPage.title}</h1>
                         {this.state.code.map(line => <h6 key={Math.random()}>{line}</h6>)}
                         {/* duplicate keys can be a problem  */}
+
+                        <h2>COMMENTS:</h2>
+                        <input type="text" style={{ width: "800px" }} value={this.state.comment} onChange={this.handleCommentChange}></input>
 
                     </div>
 
